59a25e1f86a16a1f9f6135095df69fded5643fc4
fooles <18730471450@163.com>
2017-10-26 16:39:12 +0800

    language

--- src/app/pages/pages.module.js
+++ src/app/pages/pages.module.js
@@ -13,6 +13,7 @@
             'BlurAdmin.pages.service',
             'BlurAdmin.pages.resourceMgm',
             'BlurAdmin.pages.secretDetail',
+            'BlurAdmin.pages.servicedetil',
             'BlurAdmin.pages.ui',
             'BlurAdmin.pages.login',
             'BlurAdmin.pages.components',
