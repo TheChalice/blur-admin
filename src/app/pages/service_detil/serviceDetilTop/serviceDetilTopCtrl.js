f1d8dd7b1b5c89db3b122c272bbcb36c027036cc
fooles <18730471450@163.com>
2017-11-01 13:42:06 +0800

    service detil

---
+++ src/app/pages/service_detil/serviceDetilTop/serviceDetilTopCtrl.js
@@ -0,0 +1,1 @@
+/** * Created by sorcerer on 2017/11/1. */(function () {    'use strict';    angular.module('BlurAdmin.pages.servicedetil')        .controller('serviceDetilTopCtrl', serviceDetilTopCtrl);    /** @ngInject */    function serviceDetilTopCtrl($scope) {    }})();
