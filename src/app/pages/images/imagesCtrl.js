ccd0691e6e760b03dade1f0bea02d611df5960e5
jingxy <953885034@qq.com>
2017-11-06 16:27:04 +0800

    镜像

---
+++ src/app/pages/images/imagesCtrl.js
@@ -0,0 +1,19 @@
+/**
+ * @author v.lugovsky
+ * created on 16.12.2015
+ */
+(function () {
+    'use strict';
+
+    angular.module('BlurAdmin.pages.images')
+        .controller('imagesCtrl', buildCtrl);
+
+    /** @ngInject */
+    function buildCtrl() {
+
+
+    }
+
+
+
+})();
