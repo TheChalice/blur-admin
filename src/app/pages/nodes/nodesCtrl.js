92f4c2d66893d28f35a9dfb4e41f12951b86f2b9
jiangtong <jiangtong@asisinfo.com>
2018-10-26 14:26:16 +0800

    dockerfile

--- src/app/pages/nodes/nodesCtrl.js
+++ src/app/pages/nodes/nodesCtrl.js
@@ -16,30 +16,34 @@
         //    size: 10,
         //    txt: ''
         //};
-        $scope.users = [
-            {
-                "name": "Esther Vang",
-                "value": 'qweqweqwe'
-            }, {
-                "name": "Esther Vang1",
-                "value": 'qweqweqwe'
-            }, {
-                "name": "Esther Vang2",
-                "value": 'qweqweqwe'
-            }, {
-                "name": "Esther Vang3",
-                "value": 'qweqweqwe'
-            }, {
-                "name": "Esther Vang4",
-                "value": 'qweqweqwe'
-            }
-        ];
-        console.log('nodeslist', nodeslist);
-        $scope.nodelist = nodeslist.items
+        $scope.editableTableData=[]
+        //console.log('nodeslist', nodeslist);
+        $scope.nodelist = nodeslist.items;
         $scope.$on('checkedone', function (event,item) {
-            console.log('item', item);
+            $scope.checked = item
+            //console.log('item', item);
+            var labels = [];
+            var nodeInfo = [];
+
+            angular.forEach(item.metadata.labels, function (item,i) {
+                labels.push({"name": i, "value": item})
+
+            })
+            angular.forEach(item.status.nodeInfo, function (item,i) {
+                nodeInfo.push({"name": i, "value": item})
+
+            })
+            if (item.spec&&item.spec.unschedulable) {
+                nodeInfo.unshift({"name": '是否调度', "value": '否'})
+            }else {
+                nodeInfo.unshift({"name": '是否调度', "value": '是'})
+            }
+            $scope.labels=angular.copy(labels);
+            $scope.nodeInfo=angular.copy(nodeInfo);
+
 
         })
+
         $scope.addUser = function() {
             $scope.inserted = {
                 name: '',
@@ -47,40 +51,7 @@
             };
             $scope.users.push($scope.inserted);
         };
-        //var refresh = function(page) {
-        //    var skip = (page - 1) * $scope.grid.size;
-        //    $scope.items = $scope.data.slice(skip, skip + $scope.grid.size);
-        //};
-        //获取buildConfig列表
-        //var loadBuildConfigs = function () {
-        //    //nodes.get({}, function(data){
-        //    //    $scope.nodes = data.items
-        //    //    console.log('node', $scope.nodes);
-        //    //
-        //    //}, function(res) {
-        //    //    //todo 错误处理
-        //    //});
-        //};
-        //loadBuildConfigs();
-        //根据buildConfig标签获取build列表
-        //var loadBuilds = function(items){
-        //    var labelSelector = '';
-        //    if (items.length > 0) {
-        //        labelSelector = 'buildconfig in (';
-        //        for (var i = 0; i < items.length; i++) {
-        //            labelSelector += items[i].metadata.name + ','
-        //        }
-        //        labelSelector = labelSelector.substring(0, labelSelector.length - 1) + ')';
-        //    }
-        //    Build.get({namespace: Cookie.get('namespace'),labelSelector: labelSelector}, function (data) {
-        //        //$log.info("builds", data);
-        //
-        //        $scope.resourceVersion = data.metadata.resourceVersion;
-        //        // watchBuilds(data.metadata.resourceVersion);
-        //
-        //        // fillBuildConfigs(data.items);
-        //    });
-        //};
+
 
     }
 
